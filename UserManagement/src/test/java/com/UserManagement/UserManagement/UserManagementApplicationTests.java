36ed2de63abf2791ba4623bffb0104bd27793f65
diken.dev <diken.dev@gmail.com>
2023-01-17 23:14:45 -0300

    first commit

---
+++ UserManagement/src/test/java/com/UserManagement/UserManagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserManagement.UserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
